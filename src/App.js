1bd7943199111f6e9072f0dba31b10f191b54b7f
palino87 <73043994+palino87@users.noreply.github.com>
2023-10-18 22:48:40 +0200

    initial van life

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './About'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from './Home'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <BrowserRouter>
+        <header>
+          <Link className="site-logo" to="/">#VanLife</Link>
+          <nav>
+            <Link to="/about">About</Link>
+          </nav>
+        </header>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
   );
 }
 
